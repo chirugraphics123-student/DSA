9a36294177dd7672cdac03a4ba40a0db6a65042e
Chirag Chinnappa <chiragchinnappa123@gmail.com>
2026-09-14 11:36:27 +0530

    Add Rotate_by_k implementation for array rotation

---
+++ Arrays/Rotate_by_k.java
@@ -0,0 +1,19 @@
+package Arrays;
+import java.util.Arrays;
+public class Rotate_by_k {
+    public static void rotate(int nums[],int k){
+        for(int i=1;i<=k;i++){
+            int t=nums[0];
+            for(int j=1;j<nums.length;j++){
+                nums[j-1]=nums[j];
+            }
+            nums[nums.length-1]=t;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[]={1,2,3,4,5};
+        rotate(nums, 4);
+        System.out.println(Arrays.toString(nums));
+    }
+}
